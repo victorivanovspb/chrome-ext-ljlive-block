95500f857e53b1fdf6f47b53dab1183c53781b43
Victor Ivanov <victorivanov.spb@gmail.com>
2017-04-20 22:16:40 +0400

    UPD JS: div#b-discoverytimes-wrapper
    JS-code removes the element div#b-discoverytimes-wrapper instead element div#ljlive

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-	$("div#ljlive").remove(); // удаляется элемент <div id="ljlive">
+	$("div#b-discoverytimes-wrapper").remove(); // удаляется элемент <div id="b-discoverytimes-wrapper">
+    $("ul#b-discoverytimes-items").remove(); // удаляется элемент <ul id="b-discoverytimes-items">
 });
