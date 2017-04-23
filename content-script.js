f9f9d24278364a26447f0e837acdb646ae6482e7
Victor Ivanov <victorivanov.spb@gmail.com>
2017-04-23 22:40:13 +0400

    UPD content-script.js

--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-	$("div#b-discoverytimes-wrapper").remove(); // удаляется элемент <div id="b-discoverytimes-wrapper">
+    $("div#b-discoverytimes-wrapper").remove(); // удаляется элемент <div id="b-discoverytimes-wrapper">
     $("ul#b-discoverytimes-items").remove(); // удаляется элемент <ul id="b-discoverytimes-items">
 });
